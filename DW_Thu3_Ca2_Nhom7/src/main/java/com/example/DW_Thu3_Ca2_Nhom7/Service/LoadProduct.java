8bca30e3ca5a564df82d0cf737785e0c41673b90
NDsPr <144921933+NDsPr@users.noreply.github.com>
2024-12-09 23:13:00 +0700

    cập nhật các phương thức trong class

--- DW_Thu3_Ca2_Nhom7/src/main/java/com/example/DW_Thu3_Ca2_Nhom7/Service/LoadProduct.java
+++ DW_Thu3_Ca2_Nhom7/src/main/java/com/example/DW_Thu3_Ca2_Nhom7/Service/LoadProduct.java
@@ -2,6 +2,30 @@ package com.example.DW_Thu3_Ca2_Nhom7.Service;
 
 
 public class Datawarehouse {
-	
 
+    private final ConnectionInfoService connectionInfoService;
+    private final DynamicJdbiFactory jdbiFactory;
+    private Jdbi jdbi;
+    @Autowired
+    ExtractDBInterface extractDBInterface;
+
+    public LoadProduct(ConnectionInfoService connectionInfoService, DynamicJdbiFactory jdbiFactory) {
+        this.connectionInfoService = connectionInfoService;
+        this.jdbiFactory = jdbiFactory;
+        System.out.println(LocationDataBase.EXTRACT_DATA_DB.getUrl());
+        jdbi = connectionInfoService.createConnection(1, LocationDataBase.EXTRACT_DATA_DB.getUrl(), jdbiFactory);
+    }
+
+    ;
+
+
+    public int loadFile() {
+        return extractDBInterface.loadFileToTable();
+    }
+
+    public void excute() {
+        int load = loadFile();
+
+
+    }
 }
