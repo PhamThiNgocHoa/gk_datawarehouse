87677ecb5b1ddbef5d550032f2e81224831b94d8
NDsPr <144921933+NDsPr@users.noreply.github.com>
2024-12-09 23:02:32 +0700

    --

--- DW_Thu3_Ca2_Nhom7/src/main/java/com/example/DW_Thu3_Ca2_Nhom7/Service/LoadProduct.java
+++ DW_Thu3_Ca2_Nhom7/src/main/java/com/example/DW_Thu3_Ca2_Nhom7/Service/LoadProduct.java
@@ -12,29 +12,5 @@ import com.example.DW_Thu3_Ca2_Nhom7.Repository.ExtractDBInterface;
 @Service
 public class LoadProduct {
 
-    private final ConnectionInfoService connectionInfoService;
-    private final DynamicJdbiFactory jdbiFactory;
-    private Jdbi jdbi;
-    @Autowired
-    ExtractDBInterface extractDBInterface;
-
-    public LoadProduct(ConnectionInfoService connectionInfoService, DynamicJdbiFactory jdbiFactory) {
-        this.connectionInfoService = connectionInfoService;
-        this.jdbiFactory = jdbiFactory;
-        System.out.println(LocationDataBase.EXTRACT_DATA_DB.getUrl());
-        jdbi = connectionInfoService.createConnection(1, LocationDataBase.EXTRACT_DATA_DB.getUrl(), jdbiFactory);
-    };
-
-
-
-    public int loadFile() {
-        return extractDBInterface.loadFileToTable();
-    }
-
-    public void excute() {
-        int load = loadFile();
-
-
-    }
 }
 
