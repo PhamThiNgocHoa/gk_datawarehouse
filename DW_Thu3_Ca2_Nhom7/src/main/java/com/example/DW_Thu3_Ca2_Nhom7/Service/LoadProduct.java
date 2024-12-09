de05fa5b4f1fe17c0b810084b4f71f03f255f5b3
NDsPr <144921933+NDsPr@users.noreply.github.com>
2024-12-09 14:48:14 +0700

    tạo contructor cho class LoadProduct

--- DW_Thu3_Ca2_Nhom7/src/main/java/com/example/DW_Thu3_Ca2_Nhom7/Service/LoadProduct.java
+++ DW_Thu3_Ca2_Nhom7/src/main/java/com/example/DW_Thu3_Ca2_Nhom7/Service/LoadProduct.java
@@ -2,6 +2,19 @@ package com.example.DW_Thu3_Ca2_Nhom7.Service;
 
 
 public class LoadProduct {
-	
+
+    private final ConnectionInfoService connectionInfoService;
+    private final DynamicJdbiFactory jdbiFactory;
+    private Jdbi jdbi;
+    @Autowired
+    ExtractDBInterface extractDBInterface;
+
+    public LoadProduct(ConnectionInfoService connectionInfoService, DynamicJdbiFactory jdbiFactory) {
+        this.connectionInfoService = connectionInfoService;
+        this.jdbiFactory = jdbiFactory;
+        System.out.println(LocationDataBase.EXTRACT_DATA_DB.getUrl());
+        jdbi = connectionInfoService.createConnection(1, LocationDataBase.EXTRACT_DATA_DB.getUrl(), jdbiFactory);
+    };
+
 
 }
